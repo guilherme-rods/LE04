7820190b2fd8a342ff162f348c49d365b434c37f
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-04-26 21:02:50 -0300

    Modelos Cenario01

---
+++ Cenario01/src/br/edu/up/modelos/Livro.java
@@ -0,0 +1,60 @@
+package br.edu.up.modelos;
+
+public class Livro {
+
+    private String codigo;
+
+    private String titulo;
+
+    private String[] autor;
+
+    private String isbn;
+
+    private int ano;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String[] getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String[] autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    
+
+
+
+
+}
