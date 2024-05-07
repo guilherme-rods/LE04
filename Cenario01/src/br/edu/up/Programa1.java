f657f576e079bf7c0e465849d2c82752df834abc
Guilherme Rodrigues <guilherme.g.rods@gmail.com>
2024-05-06 21:18:27 -0300

    06-05-2024 21:18 Guilherme - Instanciação e impressão de 2 livros

---
+++ Cenario01/src/br/edu/up/Programa1.java
@@ -0,0 +1,13 @@
+package br.edu.up;
+import br.edu.up.modelos.*;
+
+public class Programa1 {
+    public static void main(String[] args) throws Exception {
+        
+        Livro livro1 = new Livro("1234", "Senhor dos Aneis", new String[]{"J.R.R. Tolkien"}, "1568941201", 1954);
+        Livro livro2 = new Livro("5678", "O código da vinci", new String[]{"Dan Brown"}, "6+526651GHF", 1980);
+
+        System.out.println(livro1);
+        System.out.println(livro2);
+    }
+}
